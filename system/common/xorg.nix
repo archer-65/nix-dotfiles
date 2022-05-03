c019793d88d987ed1e17f3bdc5272a400bf9daa5
archer-65 <mariogt2009@live.it>
2022-05-03 21:41:12 +0200

    Qtile packages overlay

--- system/common/xorg.nix
+++ system/common/xorg.nix
@@ -4,6 +4,26 @@
   services = {
     dbus.enable = true;
     
+    pkgs.overlays = [
+      (self: super: {
+        qtile = super.qtile.overrideAttrs (oldAttrs: {
+          pythonPath = oldAttrs.pythonPath ++ (with self.python37Packages; [
+            keyring
+            xcffib
+            #cairocffi-xcffib
+            setuptools
+            setuptools_scm
+            dateutil
+            dbus-python
+            mpd2
+            psutil
+            pyxdg
+            pygobject3
+          ]);
+        });
+      })
+    ];
+
     xserver = {
       enable = true;
 
