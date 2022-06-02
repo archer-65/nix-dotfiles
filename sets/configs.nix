6822bd6ea73a177a02b5090bcc2d12cc46879b9c
archer-65 <mariogt2009@live.it>
2022-06-02 02:20:02 +0200

    FIX

---
+++ sets/configs.nix
@@ -0,0 +1,62 @@
+let
+  inherit (builtins) attrNames concatMap listToAttrs;
+
+  filterAttrs = pred: set:
+    listToAttrs (concatMap (name:
+      let value = set.${name};
+      in if pred name value then [{ inherit name value; }] else [ ])
+      (attrNames set));
+
+  configurations = {
+    # System configurations
+    quietfrost = {
+      type = "nixos";
+      localSystem = "x86_64-linux";
+      address = "quietfrost";
+    };
+    mate = {
+      type = "nixos";
+      localSystem = "x86_64-linux";
+      address = "mate";
+    };
+    vm = {
+      type = "nixos";
+      localSystem = "x86_64-linux";
+      address = "vm";
+    };
+
+    # Home-manager standalone
+    "mario@quietfrost" = rec {
+      type = "home-manager";
+      localSystem = "x86_64-linux";
+      username = "mario";
+      homeDirectory = home/${username};
+    };
+    "mario@mate" = rec {
+      type = "home-manager";
+      localSystem = "x86_64-linux";
+      username = "mario";
+      homeDirectory = home/${username};
+    };
+    "mario@vm" = rec {
+      type = "home-manager";
+      localSystem = "x86_64-linux";
+      username = "mario";
+      homeDirectory = home/${username};
+    };
+  };
+in {
+  all = configurations;
+
+  nixos = rec {
+    all = filterAttrs (_: v: v.type == "nixos") configurations;
+    x86_64-linux = filterAttrs (_: v: v.localSystem == "x86_64-linux") all;
+    aarch64-linux = filterAttrs (_: v: v.localSystem == "aarch64-linux") all;
+  };
+
+  homeManager = rec {
+    all = filterAttrs (_: v: v.type == "home-manager") configurations;
+    x86_64-linux = filterAttrs (_: v: v.localSystem == "x86_64-linux") all;
+    aarch64-linux = filterAttrs (_: v: v.localSystem == "aarch64-linux") all;
+  };
+}
