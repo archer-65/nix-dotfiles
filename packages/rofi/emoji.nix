5fe473a079a83cea27079fa6e7346ecab9ada3ce
archer-65 <mariogt2009@live.it>
2022-06-02 20:24:28 +0200

    emoji fix

--- packages/rofi/emoji.nix
+++ packages/rofi/emoji.nix
@@ -19,7 +19,7 @@ let rofi = "${pkgs.rofi}/bin/rofi";
 in pkgs.writeShellScriptBin "rofi_emoji" ''
   dir="$HOME/.config/rofi/themes/emoji"
 
-  ${rofi} -show emoji \
+  rofi -show emoji \
   -modi emoji \
   -theme $dir 
 ''
