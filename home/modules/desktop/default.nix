411adf3702885f86cf494d87b895aa264becb135
archer-65 <mariogt2009@live.it>
2022-06-22 18:29:55 +0200

    Assertions added

--- home/modules/desktop/default.nix
+++ home/modules/desktop/default.nix
@@ -1,39 +1,71 @@
-{ pkgs, ... }:
+# Thank you hlissner!
+{ config, options, lib, pkgs, ... }:
 
-{
-  home.packages = with pkgs; [
-    brightnessctl
-    playerctl
-    xfce.xfconf
-    xfce.exo
-    (xfce.thunar.override {
-      thunarPlugins = with pkgs; [
-        xfce.thunar-volman
-        xfce.thunar-archive-plugin
-        xfce.thunar-media-tags-plugin
-      ];
-    })
-    mate.engrampa
-    unzip
-  ];
+with lib;
+let
+  # These functions should stay in another place, but right now I need them only here.
+  attrsToList = attrs:
+    mapAttrsToList (name: value: { inherit name value; }) attrs;
+  countAttrs = pred: attrs:
+    count (attr: pred attr.name attr.value) (attrsToList attrs);
 
-  xsession.enable = true;
+  cfgXorg = config.user-modules.desktop.xorg;
+  cfgWayland = config.user-modules.desktop.wayland;
 
-  systemd.user.services = {
-    polkit = {
-      Unit = {
-        Description = "polkit-gnome";
-        Documentation = [ "man:polkit(8)" ];
-        PartOf = [ "graphical-session.target" ];
+  # Creative XOR operator :D
+  cfgExclusive = ((cfgXorg.enable || cfgWayland.enable)
+                  && (!(cfgXorg.enable && cfgWayland.enable)));
+in {
+  config = {
+    assertions = [
+      {
+        assertion = cfgExclusive;
+        message = "Can't enable customization for both Xorg and Wayland.";
+      }
+      {
+        assertion = (countAttrs (n: v: n == "enable" && v) cfgXorg) < 2;
+        message = "[Xorg] Can't have more than one WM/DE enable at a time.";
+      }
+      {
+        assertion = (countAttrs (n: v: n == "enable" && v) cfgWayland) < 2;
+        message = "[Wayland] Can't have more than one WM/DE enable at a time.";
+      }
+    ];
+
+    home.packages = with pkgs; [
+      brightnessctl
+      playerctl
+      xfce.xfconf
+      xfce.exo
+      (xfce.thunar.override {
+        thunarPlugins = with pkgs; [
+          xfce.thunar-volman
+          xfce.thunar-archive-plugin
+          xfce.thunar-media-tags-plugin
+        ];
+      })
+      mate.engrampa
+      unzip
+    ];
+
+    xsession.enable = true;
+
+    systemd.user.services = {
+      polkit = {
+        Unit = {
+          Description = "polkit-gnome";
+          Documentation = [ "man:polkit(8)" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+        Service = {
+          Type = "simple";
+          ExecStart =
+            "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          RestartSec = 3;
+          Restart = "always";
+        };
+        Install = { WantedBy = [ "graphical-session.target" ]; };
       };
-      Service = {
-        Type = "simple";
-        ExecStart =
-          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        RestartSec = 3;
-        Restart = "always";
-      };
-      Install = { WantedBy = [ "graphical-session.target" ]; };
     };
   };
 }
