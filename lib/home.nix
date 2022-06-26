6205fd3d49941ad6726f99f6e12e50e0acc02a00
archer-65 <mariogt2009@live.it>
2022-06-26 20:42:00 +0200

    New home-manager parameters

--- lib/home.nix
+++ lib/home.nix
@@ -3,21 +3,19 @@ let
   inherit (inputs.nixpkgs) lib;
   homeSet = (import ../outputs/configs.nix).homeManager.all;
 
-  genModules = home:
-    { ... }: {
-      imports = [ (import "${inputs.self}/home/configurations/${home}") ]
-        ++ [ (import "${inputs.self}/mixed/options.nix" inputs) ]
-        ++ __attrValues inputs.self.homeModules;
-    };
-
   genConfiguration = home:
     { localSystem, username, ... }:
     inputs.home-manager.lib.homeManagerConfiguration {
-      inherit username;
-      homeDirectory = "/home/${username}";
-      configuration = genModules home;
       pkgs = inputs.self.pkgs;
-      stateVersion = "22.05";
-      system = localSystem;
+      modules = [{
+        home = {
+          inherit username;
+          homeDirectory = "/home/${username}";
+          stateVersion = "22.05";
+        };
+      }] ++ [ (import "${inputs.self}/home/configurations/${home}") ]
+        ++ [ (import "${inputs.self}/mixed/options.nix" inputs) ]
+        ++ __attrValues inputs.self.homeModules;
+      # system = localSystem;
     };
 in lib.mapAttrs genConfiguration homeSet
