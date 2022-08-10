156af3cfab2a1cc4f0bb7242b50fe5d280e2302b
archer-65 <mariogt2009@live.it>
2022-08-10 13:14:47 +0200

    Removed useless thing

--- lib/nixos.nix
+++ lib/nixos.nix
@@ -3,11 +3,6 @@ let
   inherit (inputs.nixpkgs) lib;
   hosts = (import ../outputs/configs.nix).nixos.all;
 
-  netHostMap = {
-    networking.hosts =
-      lib.mapAttrs' (n: v: lib.nameValuePair v.address [ n ]) hosts;
-  };
-
   hostPkgs = { nixpkgs.pkgs = inputs.self.pkgs; };
 
   nixRegistry = { nix.registry = { nixpkgs.flake = inputs.nixpkgs; }; };
@@ -19,7 +14,6 @@ let
       specialArgs = { inherit lib inputs; };
       modules = [
         ("${inputs.self}/system/configurations/${hostname}")
-        netHostMap
         hostPkgs
         nixRegistry
         inputs.home-manager.nixosModules.home-manager
