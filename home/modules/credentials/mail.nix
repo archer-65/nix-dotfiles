1dfd630825ce00c0eef90e36c25b99d61fdce476
archer-65 <mario.liguori.056@gmail.com>
2022-10-26 20:54:22 +0200

    Removed old email addresses

--- home/modules/credentials/mail.nix
+++ home/modules/credentials/mail.nix
@@ -24,7 +24,7 @@ in {
     accounts.email.accounts = {
 
       gmailPrimary = {
-        primary = false;
+        primary = true;
         realName = "Mario Liguori";
         address = "mario.liguori.056@gmail.com";
         userName = "mario.liguori.056@gmail.com";
@@ -34,78 +34,6 @@ in {
           "${pkgs.libsecret}/bin/secret-tool lookup gmailPrimary password";
 
         maildir.path = "GmailPrimary";
-
-        mu.enable = true;
-
-        msmtp.enable = true;
-
-        mbsync = {
-          enable = true;
-          patterns = [
-            "*"
-            "![Gmail]*"
-            "[Gmail]/Trash"
-            "[Gmail]/Drafts"
-            "[Gmail]/Sent Mail"
-            "[Gmail]/Starred"
-            "[Gmail]/All Mail"
-          ];
-          create = "both";
-        };
-      };
-
-      outlook = {
-        primary = true;
-        realName = "Mario Liguori";
-        address = "mariogt2009@live.it";
-        userName = "mariogt2009@live.it";
-
-        passwordCommand =
-          "${pkgs.libsecret}/bin/secret-tool lookup live password";
-
-        maildir.path = "Outlook";
-
-        imap = {
-          host = "outlook.office365.com";
-          port = 993;
-          tls.enable = true;
-        };
-
-        smtp = {
-          host = "smtp-mail.outlook.com";
-          port = 587;
-          tls.enable = true;
-        };
-
-        mu.enable = true;
-
-        msmtp.enable = true;
-
-        mbsync = {
-          enable = true;
-          create = "both";
-          expunge = "both";
-          extraConfig = {
-            channel = { Sync = "All"; };
-            account = {
-              Timeout = 120;
-              PipelineDepth = 50;
-            };
-          };
-        };
-      };
-
-      gmail = {
-        primary = false;
-        realName = "Mario Liguori";
-        address = "mariogt2009@gmail.com";
-        userName = "mariogt2009@gmail.com";
-
-        flavor = "gmail.com";
-        passwordCommand =
-          "${pkgs.libsecret}/bin/secret-tool lookup gmail password";
-
-        maildir.path = "Gmail";
 
         mu.enable = true;
 
