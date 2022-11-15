7c95ab06735caa3ed2e31b4c52883b1724995788
archer-65 <mario.liguori.056@gmail.com>
2022-11-15 03:11:12 +0100

    Consult: enabled completion in minibuffer

--- config/emacs/lisp/init-consult.el
+++ config/emacs/lisp/init-consult.el
@@ -92,14 +92,14 @@
    consult--source-bookmark
    :preview-key (kbd "M-."))
 
-  ;; ;; Use `consult-completion-in-region' if Vertico is enabled.
-  ;; ;; Otherwise use the default `completion--in-region' function.
-  ;; (setq completion-in-region-function
-  ;; 	(lambda (&rest args)
-  ;;         (apply (if vertico-mode
-  ;;                    #'consult-completion-in-region
-  ;;                  #'completion--in-region)
-  ;; 		 args)))
+  ;; Use `consult-completion-in-region' if Vertico is enabled.
+  ;; Otherwise use the default `completion--in-region' function.
+  (setq completion-in-region-function
+	(lambda (&rest args)
+          (apply (if vertico-mode
+                     #'consult-completion-in-region
+                   #'completion--in-region)
+		 args)))
 
   ;; Optionally configure the narrowing key.
   (setq consult-narrow-key "<")
