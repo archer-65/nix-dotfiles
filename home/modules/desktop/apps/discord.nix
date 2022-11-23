c68a0b2dfa4a1eb8c2438b646b0bbebb3ff82990
archer-65 <mario.liguori.056@gmail.com>
2022-11-23 14:32:19 +0100

    Refactor discord module

--- home/modules/desktop/apps/discord.nix
+++ home/modules/desktop/apps/discord.nix
@@ -8,48 +8,49 @@
 }:
 with lib; let
   cfg = config.mario.modules.desktop.apps.discord;
-  cfgWayland = config.mario.modules.desktop.wayland;
-  inherit (inputs) webcord;
-  webcord-pkg = webcord.packages.${pkgs.system}.default;
+
+  discord-chromium = pkgs.makeDesktopItem {
+    name = "Discord";
+    desktopName = "Discord";
+    genericName = "All-in-one cross-platform voice and text chat for gamers";
+    exec = ''
+      ${config.programs.chromium.package}/bin/chromium --ozone-platform-hint=auto  --force-dark-mode --enable-features=WebUIDarkMode --app="https://discord.com/channels/@me"'';
+    icon = "discord";
+    type = "Application";
+    categories = ["Network" "InstantMessaging"];
+    terminal = false;
+    mimeTypes = ["x-scheme-handler/discord"];
+  };
 in {
   options.mario.modules.desktop.apps.discord = {
     enable = mkEnableOption "discord";
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    (mkIf cfgWayland.enable {
-      mario.modules.desktop.browsers.chromium.enable = true;
+  config = mkIf cfg.enable {
+    mario.modules.desktop.browsers.chromium.enable = true;
 
-      programs.webcord = {
-        enable = true;
-        themes = let
-          catppuccin = pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "discord";
-            rev = "159aac939d8c18da2e184c6581f5e13896e11697";
-            sha256 = "sha256-cWpog52Ft4hqGh8sMWhiLUQp/XXipOPnSTG6LwUAGGA=";
-          };
-        in {
-          CatpuccinMocha = "${catppuccin}/themes/mocha.theme.css";
+    programs.webcord = {
+      enable = true;
+      themes = let
+        catppuccin = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "discord";
+          rev = "159aac939d8c18da2e184c6581f5e13896e11697";
+          sha256 = "sha256-cWpog52Ft4hqGh8sMWhiLUQp/XXipOPnSTG6LwUAGGA=";
         };
+      in {
+        CatpuccinMocha = "${catppuccin}/themes/mocha.theme.css";
       };
+    };
 
-      home.packages = with pkgs; let
-        discord-chromium = makeDesktopItem {
-          name = "Discord";
-          desktopName = "Discord";
-          genericName = "All-in-one cross-platform voice and text chat for gamers";
-          exec = ''
-            ${config.programs.chromium.package}/bin/chromium --ozone-platform-hint=auto  --force-dark-mode --enable-features=WebUIDarkMode --app="https://discord.com/channels/@me"'';
-          icon = "discord";
-          type = "Application";
-          categories = ["Network" "InstantMessaging"];
-          terminal = false;
-          mimeTypes = ["x-scheme-handler/discord"];
-        };
-      in [discord-chromium];
-    })
+    home.packages = with pkgs; [
+      discord-chromium
 
-    (mkIf (!cfgWayland.enable) {home.packages = with pkgs; [discord];})
-  ]);
+      # Enable again when WebRTC is hooked in Pipewire
+      # (discord-canary.override {
+      #   nss = pkgs.nss_latest;
+      #   withOpenASAR = true;
+      # })
+    ];
+  };
 }
