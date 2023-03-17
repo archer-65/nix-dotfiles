93bebbc37785bbe4dc6a228ed7ddf93983fe41c6
archer-65 <mario.liguori.056@gmail.com>
2023-03-17 18:15:47 +0100

    emacs/consult: :preview-key format changed

--- home/modules/editors/emacs/config/lisp/init-consult.el
+++ home/modules/editors/emacs/config/lisp/init-consult.el
@@ -87,8 +87,7 @@
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
    consult--source-recent-file consult--source-project-recent-file
-   consult--source-bookmark
-   :preview-key (kbd "M-."))
+   consult--source-bookmark :preview-key "M-.")
 
   ;; Use `consult-completion-in-region' if Vertico is enabled.
   ;; Otherwise use the default `completion--in-region' function.
