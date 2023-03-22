1dc94972be40207af4b65af978851f41b812257c
archer-65 <mario.liguori.056@gmail.com>
2023-03-22 21:47:01 +0100

    shell(zsh): better completion

--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -24,6 +24,11 @@ in {
       enableSyntaxHighlighting = true;
       shellAliases = {};
       initExtra = ''
+	zstyle ":completion:*" menu select
+	zmodload zsh/complist
+        compinit
+        _comp_options+=(globdots)  
+
         bindkey '^[[1;5D' backward-word
         bindkey '^[[1;5C' forward-word
       '';
