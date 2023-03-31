27c85383eaa09dde2aa3346c865d247f34e2433f
archer-65 <mario.liguori.056@gmail.com>
2023-03-31 10:22:07 +0200

    emacs: out of store

--- home/modules/editors/emacs/default.nix
+++ home/modules/editors/emacs/default.nix
@@ -6,6 +6,7 @@
 }:
 with lib; let
   cfg = config.mario.modules.editors.emacs;
+  configDir = "${config.home.homeDirectory}/.dotfiles/home/modules/editors/emacs/config";
 in {
   options.mario.modules.editors.emacs = {
     enable = mkEnableOption "emacs and its configuration";
@@ -30,10 +31,12 @@ in {
           ]);
       };
 
-      xdg.configFile."emacs" = {
-        source = ./config;
-        recursive = true;
-      };
+      xdg.configFile."emacs/Emacs.org".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/Emacs.org";
+      xdg.configFile."emacs/init.el".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/init.el";
+      xdg.configFile."emacs/early-init.el".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/early-init.el";
+      xdg.configFile."emacs/lisp".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/lisp";
+      xdg.configFile."emacs/straight/version".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/straight/version";
+      xdg.configFile."emacs/img".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/img";
     }
 
     (mkIf cfg.daemon.enable {
