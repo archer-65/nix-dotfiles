a49d66e4872127e914aab2c8c2e9aa9975251050
archer-65 <mario.liguori.056@gmail.com>
2023-04-02 15:13:52 +0200

    network: fix module

--- system/modules/core/network.nix
+++ system/modules/core/network.nix
@@ -1,20 +1,20 @@
-{lib, ...}: {
+{pkgs, lib, ...}: {
   networking = {
     useDHCP = false;
     firewall.enable = true;
 
     nameservers = [
       # Quad9 v4
-      # "9.9.9.9"
-      # "149.112.112.112"
+      "9.9.9.9"
+      "149.112.112.112"
       # Quad9 v6
-      # "2620:fe::fe"
-      # "2620:fe::9"
+      "2620:fe::fe"
+      "2620:fe::9"
     ];
 
     networkmanager = {
       enable = true;
-      # dns = "none";
+      dns = "none";
     };
   };
 
@@ -22,6 +22,13 @@
     tailscale.enable = true;
   };
 
-  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
-  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+  # Don't wait for network startup
+  # https://old.reddit.com/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a
+  systemd = {
+    targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
+    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+  };
+
+  # systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  # systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 }
