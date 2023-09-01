0414fcbb6f91ace9725b58bbd669f39ce9df3454
archer-65 <mario.liguori.056@gmail.com>
2023-09-01 10:48:11 +0200

    cachix: fix trusted key for nixpkgs-wayland

--- system/modules/core/cachix.nix
+++ system/modules/core/cachix.nix
@@ -24,7 +24,7 @@ in {
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA"
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
