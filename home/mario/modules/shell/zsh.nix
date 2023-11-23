9293d32bd50c486f082e79968dc72dfc6216b829
archer-65 <mario.liguori.056@gmail.com>
2023-11-23 11:39:32 +0100

    zsh: keybinds and some neat functionalities

--- home/mario/modules/shell/zsh.nix
+++ home/mario/modules/shell/zsh.nix
@@ -23,13 +23,30 @@ in {
       syntaxHighlighting.enable = true;
       shellAliases = {};
       initExtra = ''
-        zstyle ":completion:*" menu select
-        zmodload zsh/complist
-               compinit
-               _comp_options+=(globdots)
+        # Case insensitive tab completion
+        zstyle ':completion:*' completer _complete _ignored _approximate
+        zstyle ':completion:*' list-colors '\'
+        zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+        zstyle ':completion:*' menu select
+        zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+        zstyle ':completion:*' verbose true
+        _comp_options+=(globdots)
 
-               bindkey '^[[1;5D' backward-word
-               bindkey '^[[1;5C' forward-word
+        zmodload zsh/complist
+        compinit
+
+        # Keybindings
+        bindkey '^[[1;5D' backward-word
+        bindkey '^[[1;5C' forward-word
+        bindkey '^[[3;5~' kill-word
+
+        bindkey ' ' magic-space  # [Space] - Don't do history expansion
+        
+        # Edit current command line in $EDITOR
+        autoload -U edit-command-line
+        zle -N edit-command-line
+        bindkey '^X^e' edit-command-line        
       '';
     };
   };
