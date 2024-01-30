62cae1a729d9ba79253abc069689cc296b1af778
archer-65 <mario.liguori.056@gmail.com>
2024-01-30 12:03:44 +0100

    flake: bump

--- system/modules/core/network.nix
+++ system/modules/core/network.nix
@@ -30,10 +30,8 @@
     tailscale.enable = true;
   };
 
-  # Don't wait for network startup
-  # https://old.reddit.com/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a
-  # systemd = {
-  #   targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
-  #   services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
-  # };
+  # According to 23.11 release notes, wait-online target has long been fixed.
+  # Spoiler, it's not.
+  systemd.network.wait-online.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
