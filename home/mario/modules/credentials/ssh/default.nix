88fda89ec5af5e6b21ffd14bc95ddc634fe45df1
archer-65 <mario.liguori.056@gmail.com>
2024-03-09 13:20:51 +0100

    ssh: a bit of configuration to satisfy OCD

--- home/mario/modules/credentials/ssh/default.nix
+++ home/mario/modules/credentials/ssh/default.nix
@@ -12,31 +12,36 @@ in {
   };
 
   config = mkIf cfg.enable {
+    home.file = {
+      ".ssh/keys.d/yubikey-id_ed25519.pub".text = ''
+        ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBR9GjKkCrbAbfuQJXuMTh1I6agrhiHrxlEXhWgidvLS
+      '';
+      ".ssh/sockets/.keep".text = "";
+    };
+
     sops.secrets."work/config" = {
-      sopsFile = ./work/config;
+      sopsFile = ./work/config.enc;
       mode = "0400";
       format = "binary";
       path = "${config.home.homeDirectory}/.ssh/work/config";
     };
 
-    sops.secrets."work/plugin" = {
-      sopsFile = ./work/plugin;
-      mode = "0400";
-      format = "binary";
-      path = "${config.home.homeDirectory}/.ssh/config.plugin";
+    programs.ssh = {
+      enable = true;
+      controlMaster = "auto";
+      controlPath = "~/.ssh/sockets/%r@%h-%p";
+      controlPersist = "60m";
+
+      matchBlocks."forges" = {
+        host = "github.com gitlab.*.com";
+        user = "git";
+        identitiesOnly = true;
+        identityFile = [ "~/.ssh/keys.d/yubikey-id_ed25519.pub" ];
+      };
+
+      includes = [
+        "work/config"
+      ];
     };
-
-    sops.secrets."work/ssh-route" = {
-      sopsFile = ./work/ssh-route;
-      mode = "0500";
-      format = "binary";
-      path = "${config.home.homeDirectory}/.ssh/work/ssh-route";
-    };
-
-    programs.ssh.enable = true;
-
-    programs.ssh.includes = [
-      "work/config"
-    ];
   };
 }
