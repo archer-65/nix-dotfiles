40921ec6601ca80aed0ad059fe53c4f10a0b2df8
archer-65 <mario.liguori.056@gmail.com>
2024-09-12 20:45:40 +0200

    monitoring: remove useless corectrl polkit rule as already included in the module programs

--- system/modules/hardware/monitoring.nix
+++ system/modules/hardware/monitoring.nix
@@ -19,18 +19,7 @@ in {
 
     (mkIf cfg.corectrl.enable {
       programs.corectrl.enable = true;
-
-      security.polkit.extraConfig = ''
-        polkit.addRule(function(action, subject) {
-          if ((action.id == "org.corectrl.helper.init" ||
-              action.id == "org.corectrl.helperkiller.init") &&
-              subject.local == true &&
-              subject.active == true &&
-              subject.isInGroup("users")) {
-                  return polkit.Result.YES;
-          }
-        });
-      '';
+      primaryUser.extraGroups = ["corectrl"];
     })
   ]);
 }
