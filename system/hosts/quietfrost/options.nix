e39678f92cacaa3473bb505920bfdfabcb153a52
archer-65 <mario.liguori.056@gmail.com>
2025-01-11 20:43:03 +0100

    quietfrost: try rocm

--- system/hosts/quietfrost/options.nix
+++ system/hosts/quietfrost/options.nix
@@ -55,4 +55,19 @@
   primaryUser.shell = pkgs.zsh;
 
   services.flatpak.enable = true;
+  # TODO: Move this away
+  hardware.graphics.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
+  systemd.tmpfiles.rules =
+  let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [
+        rocblas
+        hipblas
+        clr
+      ];
+    };
+  in [
+    "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+  ];
 }
